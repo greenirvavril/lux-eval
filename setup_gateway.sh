10603753c9d1cc3fdc85dbad34281779b5f2a4ab
greenirvavril <nils.rehlinger@protonmail.com>
2025-09-11 10:03:06 +0200

    Add files via upload

---
+++ setup_gateway.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Gateway setup
+cd gateway
+
+# Main environment
+python3 -m venv main_venv
+source main_venv/bin/activate
+pip install -r main_requirements.txt
+deactivate
+
+# BERTScore environment
+python3 -m venv bert_venv
+source bert_venv/bin/activate
+pip install -r bert_requirements.txt
+deactivate
+
+# BLEURT environment
+python3 -m venv bleurt_venv
+source bleurt_venv/bin/activate
+pip install -r bleurt_requirements.txt
+pip install git+https://github.com/lucadiliello/bleurt-pytorch.git
+deactivate
+
+# COMETKiwi22 environment
+python3 -m venv cometkiwi_venv
+source cometkiwi_venv/bin/activate
+pip install -r cometkiwi_requirements.txt
+git clone https://github.com/Unbabel/COMET
+cd COMET
+pip install poetry
+poetry install
+cd ..
+deactivate
+
+# Luxembedder environment
+python3 -m venv luxembedder_venv
+source luxembedder_venv/bin/activate
+pip install -r luxembedder_requirements.txt
+deactivate
+
+cd ..
